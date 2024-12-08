82d0d2e52882529187dd37c3be91ecdd4a0f6c69
Víctor Lázaro <vlazaro@outlook.es>
2024-12-08 22:02:49 +0100

    fix(shared): change aggregate root and domain event fields to private

--- backend-challenge/src/contexts/shared/domain/aggregates/aggregate_root.py
+++ backend-challenge/src/contexts/shared/domain/aggregates/aggregate_root.py
@@ -6,12 +6,12 @@ from contexts.shared.domain.events.domain_event import DomainEvent
 
 
 class AggregateRoot(BaseModel, ABC):
-    domain_events: list[DomainEvent] = Field(default=[])
+    __domain_events: list[DomainEvent] = Field(default=[])
 
     def record_domain_event(self, domain_event: DomainEvent) -> None:
-        self.domain_events.append(domain_event)
+        self.__domain_events.append(domain_event)
 
     def pull_domain_events(self) -> list[DomainEvent]:
-        domain_events = self.domain_events.copy()
-        self.domain_events.clear()
+        domain_events = self.__domain_events.copy()
+        self.__domain_events.clear()
         return domain_events
--- backend-challenge/src/contexts/shared/domain/events/domain_event.py
+++ backend-challenge/src/contexts/shared/domain/events/domain_event.py
@@ -8,11 +8,14 @@ from contexts.shared.domain.value_objects.uuid import UUID
 
 
 class DomainEvent(BaseModel, ABC):
-    event_id: UUID = Field(default_factory=uuid.uuid4())
-    ocurred_on: datetime.datetime = Field(default_factory=datetime.datetime.now())
+    __event_id: UUID = Field(default_factory=uuid.uuid4())
+    __ocurred_on: datetime.datetime = Field(default_factory=datetime.datetime.now())
 
     def get_event_id(self) -> str:
-        return str(self.event_id.value)
+        return str(self.__event_id.value)
+
+    def get_ocurred_on(self) -> str:
+        return str(self.__ocurred_on)
 
     @abstractmethod
     def get_event_name(self) -> str:
